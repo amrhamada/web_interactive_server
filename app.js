c86ef5a6f07b69d2b6910bc445d5d8e8ec076f7c
Neihy <neihy.rincon@gmail.com>
2020-06-09 12:13:28 -0400

    adding session to login and register part also adding logout

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const logger = require('morgan');
 
 //Routes
@@ -16,18 +16,22 @@ const app = express();
 const { Pool } = require('pg');
 const dbParams = require('./lib/db');
 const db = new Pool(dbParams);
-
 db.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//midleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'user-session',
+  keys: ['a5376b06-97bf-4159-b737-22642a995dd4', '16aefe91-9211-4a1b-a9dc-fc738bf89e6f']
+}));
+
 //endpoint helpers
 const teacherHelpers = require('./helpers/teacherHelpers')(db);
 
--- routes/teachersRouter.js
+++ routes/teachersRouter.js
@@ -28,6 +28,7 @@ module.exports = (dbHelpers) => {
         .json(data)
       }
       const regTeacher = data.rows;
+      req.session.teacher_id = regTeacher[0].id;
       res.json( {id:`${regTeacher[0].id}`, email:`${regTeacher[0].email}`})
     })
     .catch((err) => { 
@@ -43,23 +44,30 @@ module.exports = (dbHelpers) => {
     .then((data) => {
       const professor = data.rows;
       if (professor[0].email === teacher.email && bcrypt.compareSync(teacher.password, professor[0].password)) {
+        req.session.teacher_id = professor[0].id;
         res.json( { id:`${professor[0].id}`, 
                     first_name:`${professor[0].first_name}`,
                     last_name:`${professor[0].last_name}`,
                     email:`${professor[0].email}`,
                     avatar:`${professor[0].avatar}`,
-                  })
+                  });
       } else {
         // wrong password - Unauthorized
-        res.send(401)
+        res.sendStatus(401)
       }
     })
     .catch((err) => { 
-      res
-      .status(500)
+      res.sendStatus(400)
       .json({ error: err.message });
     });
   });
+
+  router.post("/logout", (req, res) => {
+    req.session = null;
+    res.sendStatus(200);
+  });
+
+
 
   return router;
 
