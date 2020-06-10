5246b688f557e3037d3721d79e6e464483106660
Amro <amr.hamada93@gmail.com>
2020-06-10 02:18:19 -0400

    changed gamehelpers to return the id with the game

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieSession = require('cookie-session');
 const logger = require('morgan');
 
 //Routes
@@ -16,24 +16,28 @@ const app = express();
 const { Pool } = require('pg');
 const dbParams = require('./lib/db');
 const db = new Pool(dbParams);
-
 db.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//midleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieSession({
+//   name: 'user-session',
+//   keys: ['a5376b06-97bf-4159-b737-22642a995dd4', '16aefe91-9211-4a1b-a9dc-fc738bf89e6f']
+// }));
+
 //endpoint helpers
 const teacherHelpers = require('./helpers/teacherHelpers')(db);
 const gameHelpers = require('./helpers/gameHelpers')(db);
 //endpoints
 app.use('/games', gameRouter(gameHelpers));
-app.use('/teachers', teachersRouter(teacherHelpers));
+app.use('/', teachersRouter(teacherHelpers));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
